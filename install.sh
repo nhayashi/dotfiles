17c930fd39bdef45a37f92c531694b5f6d53cabf
nhayashi <hayashi.naritoshi@dena.jp>
2021-11-04 00:45:31 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+git config --global ghq.root ~/code/src
+git config --global core.excludesfile ~/.gitignore_global
+
+rm -f ~/.dotfiles && ln -s $(ghq root)/github.com/nhayashi/dotfiles ~/.dotfiles
+
+for f in .bash_aliases .bashrc .gitignore_global .ideavimrc .tig-completion.sh .tigrc .tigrc.vim .tmux.conf; do
+    rm -f ~/$f && ln -s ~/.dotfiles/$f ~/$f
+done
+
+# TODO: configure nvim
+
+#rm -f ~/.bash_aliases      && ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+#rm -f ~/.bashrc            && ln -s ~/.dotfiles/.bashrc ~/.bashrc
+#rm -f ~/.gitignore_global  && ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+#rm -f ~/.ideavimrc         && ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
+#rm -f ~/.tig-completion.sh && ln -s ~/.dotfiles/.tig-completion.sh ~/.tig-completion.sh
+#rm -f ~/.tigrc             && ln -s ~/.dotfiles/.tigrc ~/.tigrc
+#rm -f ~/.tigrc.vim         && ln -s ~/.dotfiles/.tigrc ~/.tigrc.vim
+#rm -f ~/.tmux.conf         && ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
